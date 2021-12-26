28967019e466ed7b124f5631d7efc0c45fd549f7
avoreshin <avoreshin@gmail.com>
2021-12-26 22:54:29 +0300

    fix: add var pipex.h

--- pipex.h
+++ pipex.h
@@ -1,8 +1,21 @@
 #ifndef PIPEX_H
 # define PIPEX_H
 
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <string.h>
+
 typedef struct s_struct
 {
+	int 	fd_infile;
+	int		fd_outfile;
+	int 	fd_pipe[2];
+	int 	pid_status;
+	char 	*path;
+	char 	**cmd_arg;
 
 } t_struct;
 
